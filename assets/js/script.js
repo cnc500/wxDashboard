50bb1f82befc0dfa74281ab59efb6effb21dda42
Christian Carnevale <cncarnevale@gmail.com>
2022-04-12 23:19:57 -0400

    added header and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 $('#timer')
 console.log($('#timer'))
+
+function displayCureentDayTime() {
+    var currentDateTime = moment().format("dddd, MMMM Do, YYYY  hh:mm:ss");
+    $('#currentDayTime').text(currentDateTime);
+    }
+    // calls display displayCureentDayTime every second to display current
+    // time every second
+    setInterval(displayCureentDayTime, 1000);
