fd4ef3bf4236e48fa9eced402e66f49b1546c533
Christian Carnevale <cncarnevale@gmail.com>
2022-04-30 14:57:16 -0400

    untried lat long code written that would verify correct city in API db is chosen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,33 @@
-$('#timer')
-console.log($('#timer'))
+var searchInput = document.querySelector(".searchInput");
 
-function displayCureentDayTime() {
-    var currentDateTime = moment().format("dddd, MMMM Do, YYYY  hh:mm:ss");
-    $('#currentDayTime').text(currentDateTime);
-    }
-    // calls display displayCureentDayTime every second to display current
-    // time every second
-    setInterval(displayCureentDayTime, 1000);
+function getCityCoordinates(city){
+  var cityCoordinates = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid={d85962c1664d174ce508ae60dd8126a9}';
+  fetch(cityCoordinates)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(results){
+        var possiblePlaces = results; 
+        if (possiblePlaces.country === '') {
+            alert('Please enter a valid city in our database.');
+        
+        } else {
+            for (var i = 0; i < possiblePlaces.country.length; i++) {
+            var correctPlace = window.confirm('If your selected city is in ' + possiblePlaces.country[i] + ', then select pess OK.  Otherwise press cancel.');
+                if (correctPlace) {
+                    var lat = possiblePlaces.lat;
+                    var long = possiblePlaces.long;
+                    getWeather(lat, long);
+                    return;
+                }
+            if (i === possiblePlaces.country.length) {
+                alert('Please enter a valid city that is associated with a state or country in our database.');
+            } 
+            }
+        } 
+    })
+}
 
-    
+
+
+searchInput.addEventListener('click',getCityCoordinates);
