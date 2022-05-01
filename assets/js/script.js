c6bb08a0e3ff35dd657144f806e799b3c91e4c7e
Christian Carnevale <cncarnevale@gmail.com>
2022-05-01 11:22:48 -0400

    receiving faile to load source map error for google extension

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,47 @@
-var searchInput = document.querySelector(".searchInput");
+// var searchInput = document.querySelector(".searchInput");
+// console.log(searchInput);
 
-function getCityCoordinates(city){
-  var cityCoordinates = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid={d85962c1664d174ce508ae60dd8126a9}';
-  fetch(cityCoordinates)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(results){
-        var possiblePlaces = results; 
-        if (possiblePlaces.country === '') {
-            alert('Please enter a valid city in our database.');
-        
-        } else {
-            for (var i = 0; i < possiblePlaces.country.length; i++) {
-            var correctPlace = window.confirm('If your selected city is in ' + possiblePlaces.country[i] + ', then select pess OK.  Otherwise press cancel.');
-                if (correctPlace) {
-                    var lat = possiblePlaces.lat;
-                    var long = possiblePlaces.long;
-                    getWeather(lat, long);
-                    return;
-                }
-            if (i === possiblePlaces.country.length) {
-                alert('Please enter a valid city that is associated with a state or country in our database.');
-            } 
-            }
-        } 
-    })
-}
+// function getCityCoordinates(inputCity){
+//   var cityCoordinates = 'http://api.openweathermap.org/geo/1.0/direct?q=' + inputCity + '&limit=5&appid=f920eb96530d47ec338cb2c2b187f0b6';
+//   fetch(cityCoordinates)
+//     .then(function(response){
+//         return response.json();
+//         console.log(response.json());
+//     })
+//     .then(function(results){
+//         var possiblePlaces = results; 
+//         if (possiblePlaces.country === '') {
+//             alert('Please enter a valid city in our database.');
+//         } else {
+//             for (var i = 0; i < possiblePlaces.country.length; i++) {
+//             var correctPlace = window.confirm('If your selected city is in ' + possiblePlaces.country[i] + ', then select pess OK.  Otherwise press cancel.');
+//                 if (correctPlace) {
+//                     var lat = possiblePlaces.lat;
+//                     var lon = possiblePlaces.long;
+//                call some function to create ul for button with correct place and saving of correct place to local storage
+//                     getWeather(lat, lon);
+//                     return;
+//                 }
+//             if (i === possiblePlaces.country.length) {
+//                 alert('Please enter a valid city that is associated with a state or country in our database.');
+//             } 
+//             }
+//         } 
+//     })
+// }
+
+// function getWeather(lat, lon) {
+//     var oneWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=f920eb96530d47ec338cb2c2b187f0b6";
+//     fetch.get(oneWeather)
+//       .then(function(response){
+//         return response.json();
+//       })
+//       .then(function(data) {
+//         console.log(data);
+//         var weather = data;
+
+//       })
+// }
 
 
-
-searchInput.addEventListener('click',getCityCoordinates);
+// searchInput.addEventListener('click',getCityCoordinates);
