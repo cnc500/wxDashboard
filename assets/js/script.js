9b3800ed9e96adde52069f26024e832ec0623d92
Christian Carnevale <cncarnevale@gmail.com>
2022-05-05 23:08:50 -0400

    finished forecast weather, started saved city buttons and local storage actions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 var searchInput = document.querySelector("#inputCity");
 var searchBtn = document.querySelector("#searchBtn");
 var currentDate = [];
+var savedCities = [];
 console.log(searchInput);
+
+window.onload = displaySavedCityBtns;
+function displaySavedCityBtns() {
+    if (localStorage.getItem("savedCities")) {
+        savedCities = localStorage.getItem("savedCities");
+      } else {
+        localStorage.setItem("savedCities", '[]')
+      }
+    for (i = 0; i < savedCities.length; i++) {
+        var savedCityBtn = document.createElement('button');
+        var cityList=document.querySelector(".cityList");
+        cityList.appendChild(savedCityBtn);
+        savedCityBtn.textContent = savedCities[i];
+        savedCityBtn.setAttribute("class", "backgroundColor btn col-md-12 mt-3 mb-1");
+    
+    }
+
+}
 
 function displayCurrentDay() {
     var currentDate = moment().format("MMMM Do, YYYY");
@@ -23,11 +42,13 @@ function getCityCoordinates(inputCity){
             if (possiblePlaces.country === '') {
                 alert('Please enter a valid city in our database.');
             } else {
+                saveCity(inputCity);
                 for (var i = 0; i < possiblePlaces.length; i++) {
                 var correctPlace = window.confirm('If your selected city is in ' + possiblePlaces[i].state + ', then select pess OK.  Otherwise press cancel.');
                     if (correctPlace) {
                         var lat = possiblePlaces[i].lat;
                         var lon = possiblePlaces[i].lon;
+
                 //    call some function to create ul for button with correct place and saving of correct place to local storage
                         getWeather(lat, lon);
                         return;
@@ -68,9 +89,9 @@ function getWeather(lat, lon) {
             if (uvIndex<3) {
                 currentUV.setAttribute("style", "color: white; background-color: green");
             } else if (uvIndex>=3 && uvIndex<6) {
-                currentUV.setAttribute("style", "color: white; background-color: yellow");
+                currentUV.setAttribute("style", "color: blue; background-color: yellow");
             } else {
-                currentUV.setAttribute("style", "color: white; display: block; background-color: red");
+                currentUV.setAttribute("style", "color: white; background-color: red");
             }
             console.log(uvIndex);
             var iconUrl="http://openweathermap.org/img/wn/"+weather.current.weather[0].icon+".png";
@@ -85,9 +106,7 @@ function forecast(daily) {
     console.log(daily);
     for (var i=1; i<6; i++) {
         console.log("daily:");
-
         console.log(daily[i].dt);
-
         var date = moment.unix(daily[i].dt).format("MMMM Do, YYYY");
         console.log(date);
         console.log(document.getElementsByClassName("forecastDate")[i-1]);
@@ -95,9 +114,38 @@ function forecast(daily) {
         forecastBox.textContent=date;
         // let date = daily[i].
         // var forecastDate = moment().format("MMMM Do, YYYY");
-  
+        var forecastWx = document.getElementsByClassName("forecastWxIcon")[i-1];
+        console.log(daily[i].weather[0]);
+        var forecastIconUrl="http://openweathermap.org/img/wn/"+daily[i].weather[0].icon+".png";
+        forecastWx.setAttribute("src", forecastIconUrl);
+        var forecastTemperature = document.getElementsByClassName("forecastTemperature")[i-1];
+        forecastTemperature.textContent="Temp " + daily[i].temp.day + " °F";
+        var forecastHumidity = document.getElementsByClassName("forecastHumidity")[i-1];
+        forecastHumidity.textContent="Humidity: " + daily[i].humidity + " %";
+        var forecastWind = document.getElementsByClassName("forecastWind")[i-1];
+        forecastWind.textContent="Wind: " + daily[i].wind_speed + " MPH";
+
     }
 }
+
+
+function saveCity(inputCity) {
+    console.log(inputCity);
+    if (!savedCities.includes(inputCity)) {
+        var newCity = inputCity;
+        savedCities.push(newCity);
+        console.log(savedCities);
+    }
+    localStorage.setItem("savedCities",inputCity);
+    // localStorage.getItem("inputCity");
+    // console.log(localStorage.getItem("inputCity"));
+    // for (i = 0; i < savedCities.length; i++) {}
+    var savedCityBtn = document.createElement('button');
+    var cityList=document.querySelector(".cityList");
+    cityList.appendChild(savedCityBtn);
+    savedCityBtn.textContent =inputCity;
+    savedCityBtn.setAttribute("class", "backgroundColor btn col-md-12 mt-3 mb-1");
+ }
 
 searchBtn.addEventListener("click",function(event){
     event.preventDefault();
