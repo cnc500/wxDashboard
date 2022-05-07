67d6a9cc5ea242ac87767cbbdf793512a416fa69
Christian Carnevale <cncarnevale@gmail.com>
2022-05-07 13:46:57 -0400

    worked on saveCity and displaySavedCityBtns functions, added comments for each function, cleaned up html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,72 @@
 var searchInput = document.querySelector("#inputCity");
 var searchBtn = document.querySelector("#searchBtn");
+var searchCity = [];
 var currentDate = [];
 var savedCities = [];
+var storedCities = [];
 console.log(searchInput);
 
+// Upon startup, this function is executed to retrieve cities saved in local storage and store
+// to display the as buttons beneath the city search butoon.
 window.onload = displaySavedCityBtns;
 function displaySavedCityBtns() {
-    if (localStorage.getItem("savedCities")) {
-        savedCities = localStorage.getItem("savedCities");
-      } else {
-        localStorage.setItem("savedCities", '[]')
-      }
+    loadSavedCities();
     for (i = 0; i < savedCities.length; i++) {
         var savedCityBtn = document.createElement('button');
         var cityList=document.querySelector(".cityList");
         cityList.appendChild(savedCityBtn);
         savedCityBtn.textContent = savedCities[i];
         savedCityBtn.setAttribute("class", "backgroundColor btn col-md-12 mt-3 mb-1");
-    
+        // savedCityBtn.addEventListener("click",function(event) {
+        //     event.preventDefault();
+        //     var buttonCity = savedCities[i];
+        //     getCityCoordinates(buttonCity);
+        // })
     }
 
 }
 
+// window.onload = displaySavedCityBtns;
+// function displaySavedCityBtns() {
+//     loadStoredCities();
+//     for (i = 0; i < storedCities.length; i++) {
+//         var savedCityBtn = document.createElement('button');
+//         var cityList=document.querySelector(".cityList");
+//         cityList.appendChild(savedCityBtn);
+//         savedCityBtn.textContent = storedCities[i];
+//         savedCityBtn.setAttribute("class", "backgroundColor btn col-md-12 mt-3 mb-1");
+//         savedCityBtn.addEventListener("click",function(event) {
+//             event.preventDefault();
+//             var buttonCity = storedCities[i];
+//             getCityCoordinates(buttonCity);
+//         })
+//     }
+
+// }
+
+// function loadStoredCities() {
+//     if (localStorage.getItem("savedCities")) {
+//         var savedCitiesStr=localStorage.getItem("savedCities");
+//         storedCities = JSON.parse(savedCitiesStr);
+//         console.log(storedCities);
+//       } else {
+//         storedCities = [];
+//         localStorage.setItem("savedCities", JSON.stringify(storedCities));
+//       }
+//     }
+    
+//  This function retrieves and texts the current date in the current weather information box.
 function displayCurrentDay() {
     var currentDate = moment().format("MMMM Do, YYYY");
     today.textContent=currentDate;
     console.log(currentDate);
-    // return currentDate;
     }
 
+// Using the OpenWeather API, this function retrieves the latitude and longitude of the city 
+// that the user inputs provided that the input city is a valid city. It calls both the saveCity 
+// and the getWeather functions.
 function getCityCoordinates(inputCity){
   var cityCoordinates = 'http://api.openweathermap.org/geo/1.0/direct?q=' + inputCity + '&limit=5&appid=f920eb96530d47ec338cb2c2b187f0b6';
-//   var cityCoordinates = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=f920eb96530d47ec338cb2c2b187f0b6';
-
   fetch(cityCoordinates)
     .then(function(response){
         response.json()
@@ -41,6 +75,7 @@ function getCityCoordinates(inputCity){
             console.log(possiblePlaces);
             if (possiblePlaces.country === '') {
                 alert('Please enter a valid city in our database.');
+                // return;
             } else {
                 saveCity(inputCity);
                 for (var i = 0; i < possiblePlaces.length; i++) {
@@ -48,8 +83,8 @@ function getCityCoordinates(inputCity){
                     if (correctPlace) {
                         var lat = possiblePlaces[i].lat;
                         var lon = possiblePlaces[i].lon;
+                        searchCity = inputCity;
 
-                //    call some function to create ul for button with correct place and saving of correct place to local storage
                         getWeather(lat, lon);
                         return;
                     }
@@ -62,7 +97,6 @@ function getCityCoordinates(inputCity){
     })
 }
 
-// getCityCoordinates();
 var validatedCity = document.querySelector(".cityName");
 var today = document.querySelector(".today")
 var currentTemperature= document.querySelector(".currentTemperature");
@@ -71,6 +105,13 @@ var currentWind = document.querySelector(".currentWind");
 var currentUV = document.querySelector(".currentUV");
 var currentIcon = document.querySelector(".currentWx");
 
+// Using the OpenWeather API, the getweather function retrieves the weather information for the 
+// latitude and longitude of the city selected.  This function also displays the current weather 
+// information for the selected city including an appropriate weather icon also retrieved from 
+// the OpenWeather API.  The UVI index retrieved is also color coded indicating whether the 
+// UVI conditions are conditions are favorable (UVI less than 3), moderate (UVI from 3 up to 6), 
+// or severe (UVI 6 and above). Finally, the getWeather function calls the forecast function 
+// and passes into it the selected city's weather information. 
 function getWeather(lat, lon) {
     var oneWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=f920eb96530d47ec338cb2c2b187f0b6";
     fetch(oneWeather)
@@ -79,7 +120,7 @@ function getWeather(lat, lon) {
          .then(function(data) {
             console.log(data);
             var weather = data;
-            validatedCity.textContent=searchInput.value;
+            validatedCity.textContent=searchCity;
             displayCurrentDay();
             currentTemperature.textContent="Temp " + weather.current.temp + " °F";
             currentHumidity.textContent="Humidity: " + weather.current.humidity + " %";
@@ -101,7 +142,9 @@ function getWeather(lat, lon) {
       })
 }
 
-
+// The forecast function displays the next 5 days of forecasted weather information for the 
+// selected city including an appropriate weather icon for each day forecasted from the 
+// OpenWeather API. 
 function forecast(daily) { 
     console.log(daily);
     for (var i=1; i<6; i++) {
@@ -112,8 +155,6 @@ function forecast(daily) {
         console.log(document.getElementsByClassName("forecastDate")[i-1]);
         var forecastBox = document.getElementsByClassName("forecastDate")[i-1];
         forecastBox.textContent=date;
-        // let date = daily[i].
-        // var forecastDate = moment().format("MMMM Do, YYYY");
         var forecastWx = document.getElementsByClassName("forecastWxIcon")[i-1];
         console.log(daily[i].weather[0]);
         var forecastIconUrl="http://openweathermap.org/img/wn/"+daily[i].weather[0].icon+".png";
@@ -128,25 +169,64 @@ function forecast(daily) {
     }
 }
 
-
+//  Ater calling the function, loadSavedCities, the saveCity function checks to see if the 
+//  input city has already been already saved in local storage or not. If it is not in local
+//  storage, than it adds it to local storage as well as creates a button that the user can
+//  click on in the future to call for that city's weather. Finally, it limits the number of 
+//  cities that can be displayed and clicked on to 12. When an 13th city is saved, it deletes 
+//  the oldest saved city button and the oldest city itself from local storage. 
 function saveCity(inputCity) {
     console.log(inputCity);
+    loadSavedCities();
     if (!savedCities.includes(inputCity)) {
         var newCity = inputCity;
+        console.log(newCity);
         savedCities.push(newCity);
+        localStorage.setItem("savedCities", JSON.stringify(savedCities));
+ 
         console.log(savedCities);
-    }
-    localStorage.setItem("savedCities",inputCity);
+    
+    // localStorage.setItem("savedCities",inputCity);
     // localStorage.getItem("inputCity");
     // console.log(localStorage.getItem("inputCity"));
     // for (i = 0; i < savedCities.length; i++) {}
-    var savedCityBtn = document.createElement('button');
-    var cityList=document.querySelector(".cityList");
-    cityList.appendChild(savedCityBtn);
-    savedCityBtn.textContent =inputCity;
-    savedCityBtn.setAttribute("class", "backgroundColor btn col-md-12 mt-3 mb-1");
- }
+        var savedCityBtn = document.createElement('button');
+        var cityList=document.querySelector(".cityList");
+        cityList.appendChild(savedCityBtn);
+        savedCityBtn.textContent = inputCity;
+        savedCityBtn.addEventListener("click",function(event) {
+            event.preventDefault();
+            var buttonCity = inputCity;
+            getCityCoordinates(buttonCity);
+        })
+        savedCityBtn.setAttribute("class", "backgroundColor btn col-md-12 mt-3 mb-1");
+    // savedCityBtn.setAttribute("id", inputCity );
+    // savedCityBtn.setAttribute("on-click", getCityCoordinates(inputCity) );
+    }
+    if (savedCities.length > 3) {
+    savedCities.shift();
+        localStorage.setItem("savedCities", JSON.stringify(savedCities));
+        cityList.innerHTML = '';
+        displaySavedCityBtns();
+    }
+}
 
+//  If there are cities saved in local storage on the user's computer, then it retrieves 
+//  them into the savedCities array.  If there are no cities saved in local storage on the 
+//  user's computer, then it creates an empty array called savedCities. 
+function loadSavedCities() {
+if (localStorage.getItem("savedCities")) {
+    var savedCitiesStr=localStorage.getItem("savedCities");
+    savedCities = JSON.parse(savedCitiesStr);
+    console.log(savedCities);
+  } else {
+    savedCities = [];
+    localStorage.setItem("savedCities", JSON.stringify(savedCities));
+  }
+}
+
+//  When the serach button is clicked, the getCityCoordinates function is called passing
+//  into that function the user's input typed into the input box above the search button.
 searchBtn.addEventListener("click",function(event){
     event.preventDefault();
     getCityCoordinates(searchInput.value);
